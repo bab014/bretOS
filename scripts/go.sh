eaae6a1b8da9aeea799a6f3d78de53d1fef4121e
Bret Beatty <bbeatty14@gmail.com>
2023-12-26 12:46:57 -0600

    needed to add -O to wget command

--- scripts/go.sh
+++ scripts/go.sh
@@ -3,7 +3,7 @@
 DOWNLOAD_DIR="$HOME/Downloads"
 
 # downloads and install go
-wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz "$DOWNLOAD_DIR/go.tar.gz"
+wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz -O "$DOWNLOAD_DIR/go.tar.gz"
 
 # extract the tar file to /usr/local
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "$DOWNLOAD_DIR/go.tar.gz"

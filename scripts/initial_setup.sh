754229baad04a55280dc57aed4cbe79022ef3c3b
Bret Beatty <bbeatty14@gmail.com>
2023-12-29 15:10:24 -0600

    added feh and sxiv to installed packages

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -15,7 +15,7 @@ mkdir $REPO_DIR
 
 # installing flatpak and other things
 sudo apt update
-sudo apt install flatpak curl wget htop neofetch unzip python3-venv python3-pip file xsel
+sudo apt install flatpak curl wget htop neofetch unzip python3-venv python3-pip file xsel feh sxiv
 
 # setup the flathub repo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
